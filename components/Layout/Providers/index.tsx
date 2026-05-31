dde3fabc3f4b3a4a943d8dd4d66229b615096e20
AssoDePicche <samuelprado730@gmail.com>
2026-05-31 13:11:56 -0300

    refactor: compose providers

--- components/Layout/Providers/index.tsx
+++ components/Layout/Providers/index.tsx
@@ -4,17 +4,29 @@ import { FC, PropsWithChildren, ReactNode } from 'react';
 
 import StyledComponentsRegistry from '@lib/registry';
 
-export const Providers: FC<PropsWithChildren> = ({ children }): ReactNode => {
+const compose = (providers: FC<PropsWitChildren>[]): ReactNode => providers.reduce((Previous, Current) => ({ children }: PropsWithChildren) => {
+  if (!Previous) return <Current>{children}</Current>;
+
   return (
-    <>
-      <ThemeProvider
-        attribute="class"
-        defaultTheme="dark"
-        disableTransitionOnChange
-        enableSystem
-      >
-        <StyledComponentsRegistry>{ children }</StyledComponentsRegistry>
-      </ThemeProvider>
-    </>
+    <Previous>
+      <Current>{children}</Current>
+    </Previous>
+  );
+});
+
+const Theme: FC<PropsWithChildren> = ({ children }): ReactNode => {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="dark"
+      disableTransitionOnChange
+      enableSystem
+    >
+      <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+    </ThemeProvider>
   );
 };
+
+export const Providers: ReactNode = compose([
+  Theme,
+]);
